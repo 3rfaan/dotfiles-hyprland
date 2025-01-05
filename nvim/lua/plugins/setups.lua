2906a476eefc5600db4ed047a266587ab62c24e1
3rfaan <zubi.arfan@gmail.com>
2025-01-05 22:33:50 +0100

    Ignore written messages in noice

--- nvim/lua/plugins/setups.lua
+++ nvim/lua/plugins/setups.lua
@@ -42,6 +42,16 @@ require("colorizer").setup()
 
 -- Noice
 require("noice").setup({
+    routes = {
+        {
+            filter = {
+                event = "msg_show",
+                kind = "",
+                find = "written",
+            },
+            opts = { skip = true },
+        },
+    },
     lsp = {
         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
         override = {
